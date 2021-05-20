6721eb2f2b50737c0f3e968e660ee2a6ef0a1ec1
mari-mizutani <82095084+mari-mizutani@users.noreply.github.com>
2021-05-20 09:37:53 +0200

    first commit

---
+++ Sites/becode/php/2.debugging/resources/index.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+// Below are several code blocks, read them, understand them and try to find whats wrong.
+// Once this exercise is finished, we'll go over the code all together and we can share how we debugged the following problems.
+// Try to fix the code every time and good luck ! (write down how you found out the answer and how you debugged the problem)
+// =============================================================================================================================
+
+// === Exercise 1 ===
+// Below we're defining a function, but it doesn't work when we run it.
+// Look at the error you get, read it and it should tell you the issue...,
+// sometimes, even your IDE can tell you what's wrong
+echo "Exercise 1 starts here:";
+
+function new_exercise() {
+    $block = "<br/><hr/><br/><br/>Exercise $x starts here:<br/>";
+    echo $block;
+
+}
+
+
